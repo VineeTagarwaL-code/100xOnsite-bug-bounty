3dd7fda3319670eaa2439f41dcb7edb79f81fead
Ankur02Sarkar <sarkarrohonankur@gmail.com>
2024-08-22 19:48:40 +0530

    fix todos not found error

--- component/TodoList.js
+++ component/TodoList.js
@@ -2,9 +2,11 @@ import React from "react";
 import TodoItem from "./TodoItem";
 
 const TodoList = ({ todos, onToggleComplete, onDelete }) => {
+  console.log("todos : ", todos);
+  
   return (
     <ul className="list-none p-0">
-      {todos.map((todo) => (
+      {todos?.map((todo) => (
         <TodoItem
           key={todo.id}
           todo={todo}
