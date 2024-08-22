e7a0d65bafdb954183e61bf590795c9fbef8731b
Milind Mishra <milind.mishra4@gmail.com>
2024-08-22 19:42:52 +0530

    📦 NEW: fix classnames

--- app/page.tsx
+++ app/page.tsx
@@ -1,37 +1,32 @@
 //@ts-nocheck
-"use client";
+'use client'
 
-import TodoForm from "../component/TodoForm";
-import TodoList from "../component/TodoList";
-import { useState } from "react";
+import TodoForm from '../component/TodoForm'
+import TodoList from '../component/TodoList'
+import { useState } from 'react'
 
 export default function Home() {
-  const [todos, setTodos] = useState();
+  const [todos, setTodos] = useState()
 
-  const addTodo = (text) => {
-    const newTodo = {
-      id: Math.random(),
-      text,
-      completed: false,
-    };
-    setTodos([...todos, newTodo]);
-  };
+  const addTodo = (newTodo: Todo) => {
+    setTodos([...todos, newTodo])
+  }
 
-  const toggleComplete = (id) => {
+  const toggleComplete = (id: number) => {
     setTodos(
       todos.map((todo) =>
         todo.id === id ? { ...todo, completed: !todo.completed } : todo
       )
-    );
-  };
+    )
+  }
 
-  const deleteTodo = (id) => {
-    setTodos(todos.filter((todo) => todo.id != id));
-  };
+  const deleteTodo = (id: number) => {
+    setTodos(todos.filter((todo) => todo.id !== id))
+  }
 
   return (
-    <div class="container mx-auto px-4 py-8">
-      <h1 class="text-3xl font-bold mb-6">Todo List</h1>
+    <div className="container mx-auto px-4 py-8">
+      <h1 className="text-3xl font-bold mb-6">Todo List</h1>
       <TodoForm onAddTodo={addTodo} />
       <TodoList
         todos={todos}
@@ -39,5 +34,5 @@ export default function Home() {
         onDelete={deleteTodo}
       />
     </div>
-  );
+  )
 }
